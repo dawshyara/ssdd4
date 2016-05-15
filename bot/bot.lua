b17c8955b889e6a0d171d24c34abe86a4d1b49d8
dawshyara <dawsh.saman@gmail.com>
2016-05-16 02:52:35 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {our_id},
+    sudo_users = {152046992},
     admin_users = {},
     disabled_channels = {}
   }
